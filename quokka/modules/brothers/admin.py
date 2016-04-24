9df96d0a0dacfbf19750007cad113c5fea3d2cb8
kekedeng <kekedeng@iZ28jks9wulZ.(none)>
2016-04-24 22:34:00 +0800

    * Complete models about brothers. * Complete five page templates about brothers.

--- quokka/modules/brothers/admin.py
+++ quokka/modules/brothers/admin.py
@@ -3,16 +3,16 @@
 from quokka import admin
 from quokka.core.admin.models import BaseContentAdmin
 from quokka.core.widgets import TextEditor, PrepopulatedText
-from .models import BrotherVideo, Experience, BrotherInfo, BrotherArticle
+from .models import BrotherVideos, BrotherInfo, BrotherArticles
 from quokka.utils.translation import _l
 
-class BrotherInfoAdmin(BaseContentAdmin):
+class BrotherInfoAdmin(BaseContentAdmin): 
 
     form_columns = [
+        'channel',
+        'related_channels',
         'title',
         'slug',
-        'channel',
-        'brother_name', 
         'brother_college_major', 
         'brother_college_name',
         'brother_tags',
@@ -20,10 +20,43 @@ class BrotherInfoAdmin(BaseContentAdmin):
         'brother_experience_study',
         'brother_experience_work',
         'brither_experience_award',
-        'tags',
+        'add_image',
         'brother_is_video',
-        'brother_video',
+        'brother_videos',
         'brother_article',
+        'published',
     ]
 
+    form_args = {
+        'slug': {'widget': PrepopulatedText(master='title')}
+    }
+
+class BrotherVideosAdmin(BaseContentAdmin):
+    
+    form_columns = [
+        'channel',
+        'title',
+        'slug',
+        'videos',
+    ]
+
+    form_args = {
+        'slug': {'widget': PrepopulatedText(master='title')}
+    }
+
+class BrotherArticlesAdmin(BaseContentAdmin):
+    
+    form_columns = [
+        'channel',
+        'title',
+        'slug',
+        'articles',
+    ]
+    
+    form_args = {
+        'slug': {'widget': PrepopulatedText(master='title')}
+    }
+
 admin.register(BrotherInfo, BrotherInfoAdmin, category=_l("Content"), name=_l("BrotherInfo"))
+admin.register(BrotherVideos, BrotherVideosAdmin, category=_l("Content"), name=_l("BrotherVideos"))
+admin.register(BrotherArticles, BrotherArticlesAdmin, category=_l("Content"), name=_l("BrotherArticles"))
--- quokka/modules/brothers/models.py
+++ quokka/modules/brothers/models.py
@@ -4,6 +4,34 @@ from quokka.core.db import db
 from quokka.core.models.content import Content
 from quokka.core.models.subcontent import SubContent
 
+class Video(db.EmbeddedDocument):
+    """
+    单一视频
+    """
+    video_title = db.StringField(max_length=255, required=True) # 视频标题
+    video_url = db.StringField(max_length=255, required=True) # 视频url
+
+class BrotherVideos(Content):
+    """
+    师兄视频集
+    """
+    videos = db.ListField(
+        db.EmbeddedDocumentField(Video, required=True), required=False) # 师兄视频列表
+
+class Article(db.EmbeddedDocument):
+    """
+    师兄单一文章
+    """
+    title = db.StringField(max_length=255, required=True) # 文章标题
+    body = db.StringField(required=True) # 文章正文
+
+class BrotherArticles(Content):
+    """
+    师兄文章集
+    """
+    articles = db.ListField(
+        db.EmbeddedDocumentField(Article, required=True), required=False) # 师兄文章列表
+
 class Experience(db.EmbeddedDocument):
     """
     个人经历信息
@@ -11,42 +39,24 @@ class Experience(db.EmbeddedDocument):
     title = db.StringField(max_length=255, required=True) # 名称
     time = db.StringField(max_length=255, required=True) # 时间区间
 
-class BrotherVideo(db.EmbeddedDocument):
-    """
-    视频型师兄信息，含五个视频URL
-    """ 
-    brother_video1_url = db.StringField(max_length=255, required=False)
-    brother_video2_url = db.StringField(max_length=255, required=False)
-    brother_video3_url = db.StringField(max_length=255, required=False)
-    brother_video4_url = db.StringField(max_length=255, required=False)
-    brother_video5_url = db.StringField(max_length=255, required=False)
-
-class BrotherArticle(db.EmbeddedDocument):
-    """
-    文章型师兄信息
-    TODO: Complete it.
-    """
-    pass
-
 class BrotherInfo(Content):
     """
     师兄信息
     """
-    brother_name = db.StringField(max_length=255, required=False) # 师兄姓名
-    brother_college_major = db.StringField(max_length=255, required=False) # 师兄专业
-    brother_college_name = db.StringField(max_length=255, required=False) # 师兄毕业院校
-    brother_tags = db.ListField(db.StringField(max_length=255)) # 师兄标签
-    brother_motto = db.StringField(max_length=255, required=False) # 师兄语录
+    # 师兄封面图片，使用Content.path
+    # 师兄姓名，使用Content.title
+    brother_college_major = db.StringField(max_length=255, required=True) # 师兄专业
+    brother_college_name = db.StringField(max_length=255, required=True) # 师兄毕业院校
+    brother_tags = db.ListField(db.StringField(max_length=255), required=False) # 师兄标签
+    brother_motto = db.StringField(max_length=255, required=True) # 师兄语录
     brother_experience_study = db.ListField(
         db.EmbeddedDocumentField(Experience), required=False) # 师兄学习经历
     brother_experience_work = db.ListField(
         db.EmbeddedDocumentField(Experience), required=False) # 师兄工作经历
     brither_experience_award = db.ListField(
         db.EmbeddedDocumentField(Experience), required=False) # 师兄获奖经历
-    brother_cover_small = db.StringField(max_length=255, required=False) # 师兄小封面
-    brother_cover_middle = db.StringField(max_length=255, required=False) # 师兄中封面
-    brother_cover_large = db.StringField(max_length=255, required=False) # 师兄大封面
-    brother_is_video = db.BooleanField(required=True) # 是否视频类型
-    brother_video = db.EmbeddedDocumentField(BrotherVideo, required=False) # 视频信息
-    brother_article = db.EmbeddedDocumentField(BrotherArticle, required=False) # 文章信息
+    brother_is_video = db.BooleanField(required=False, default=True) # 是否视频类型
+    brother_videos = db.ReferenceField(BrotherVideos, required=False) # 视频
+    brother_article = db.ReferenceField(BrotherArticles, required=False) # 文章信息
+
 
