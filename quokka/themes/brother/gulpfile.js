adb5d96fcf1070530a6f614f9de86bf5be3ee369
horsley <mail@horsley.me>
2016-05-25 21:02:48 +0800

    Fix gulp problems.

--- quokka/themes/brother/gulpfile.js
+++ quokka/themes/brother/gulpfile.js
@@ -4,24 +4,24 @@ var rev = require('gulp-rev'); // 对文件名加MD5后缀
 var revCollector = require('gulp-rev-collector'); // 模板路径替换
 var debug = require('gulp-debug'); // DBEUG
 
-gulp.task('concat', function() {                         
-    gulp.src(['./static/css/*.css']) 
+gulp.task('rev', function() {                         
+    return gulp.src(['static/css/*.css']) 
         // 文件名加MD5后缀  
         .pipe(rev())        
         // 输出文件本地                                   
-        .pipe(gulp.dest('./static/css'))                             
+        .pipe(gulp.dest('static/css'))    
         // 生成一个rev-manifest.json
         .pipe(rev.manifest())                                  
         // 将 rev-manifest.json 保存到 rev 目录内
-        .pipe(gulp.dest('./rev'));                              
+        .pipe(gulp.dest('rev'));                              
 });
 
-gulp.task('rev', function() {
-    gulp.src(['./rev/*.json', './templates/*.html'])  
+gulp.task('rev_template', function() {
+    return gulp.src(['dist/*.json', 'templates/**/*.html'])
         // 替换模板中文件名
         .pipe(revCollector())
         // 输出文件
-        .pipe(gulp.dest('./templates/')).debug();
+        .pipe(gulp.dest('templates'));
 });
 
-gulp.task('default', ['concat', 'rev']);
+gulp.task('default', ['rev', 'rev_template']);
