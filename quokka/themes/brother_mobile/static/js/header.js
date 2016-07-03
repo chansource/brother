b776dac95dc0303663fe324638b5e6e14bf3cb5c
dengke <289687761@qq.com>
2016-07-03 12:16:07 +0800

    完成微博分享

--- quokka/themes/brother_mobile/static/js/header.js
+++ quokka/themes/brother_mobile/static/js/header.js
@@ -11,3 +11,19 @@ $("#nav-btn").on('tap',function (){
     navStatus=!navStatus;
     shareStatus=!shareStatus;
 });
+
+
+var shareLink = location.href;
+var shareTitle = document.title;
+$(".share-btn").on("tap", function(event) {
+    window.location.href = "http://v.t.sina.com.cn/share/share.php?title=" + encodeURIComponent(shareTitle) + "&url=" + encodeURIComponent(shareLink);
+});
+// $(".social-share-tweibo").click(function () {
+//     window.open(
+//         "http://share.v.t.qq.com/index.php?c=share&a=index&url=" + encodeURIComponent(shareLink)+"&title=" +encodeURIComponent(shareTitle)
+//     );
+// });
+// $(".social-share-qzone").click(function () {
+//     window.open('http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url='+ encodeURIComponent(shareLink)+ '&title='+encodeURIComponent(shareTitle)
+//     );
+// });
