69775977a1ce5cbb7ecf5a436e09e98355d9028b
chansource <kahouchan@163.com>
2016-07-17 18:00:03 +0800

    隐藏header中的分享按钮，去除按钮事件

--- quokka/themes/brother_mobile/static/js/header.js
+++ quokka/themes/brother_mobile/static/js/header.js
@@ -1,23 +1,23 @@
 "use strict";
 var navStatus=true;
-var shareStatus=true;
+// var shareStatus=true;
 
 $("#nav-btn").on('tap',function (){
-    $("#share-btn img").toggle();
+    // $("#share-btn img").toggle();
     $(".page-nav").toggle();
     $("#nav-btn img").toggle();
     $(".help-btn").toggle();
     $(".fix-btn").toggle();
     navStatus=!navStatus;
-    shareStatus=!shareStatus;
+    // shareStatus=!shareStatus;
 });
 
 
-var shareLink = location.href;
-var shareTitle = document.title;
-$(".share-btn").on("tap", function(event) {
-    window.location.href = "http://v.t.sina.com.cn/share/share.php?title=" + encodeURIComponent(shareTitle) + "&url=" + encodeURIComponent(shareLink);
-});
+// var shareLink = location.href;
+// var shareTitle = document.title;
+// $(".share-btn").on("tap", function(event) {
+//     window.location.href = "http://v.t.sina.com.cn/share/share.php?title=" + encodeURIComponent(shareTitle) + "&url=" + encodeURIComponent(shareLink);
+// });
 // $(".social-share-tweibo").click(function () {
 //     window.open(
 //         "http://share.v.t.qq.com/index.php?c=share&a=index&url=" + encodeURIComponent(shareLink)+"&title=" +encodeURIComponent(shareTitle)
